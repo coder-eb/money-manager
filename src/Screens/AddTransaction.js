32c5785ef4c9099e0d95e9cbb7274821d749bf45
lazy_eb <ebranbright@gmail.com>
2023-04-07 20:11:53 +0530

    Dark style changes in Add Transaction Screen

--- src/Screens/AddTransaction.js
+++ src/Screens/AddTransaction.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons"
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { format } from 'date-fns'
+import Icon from "react-native-vector-icons/MaterialIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { format } from "date-fns";
 
 const AddTransaction = () => {
     const [type, setType] = useState("Expense");
@@ -10,26 +10,27 @@ const AddTransaction = () => {
     const [description, setDescription] = useState("");
     const [amount, setAmount] = useState("");
     const [date, setDate] = useState(new Date());
-    const [mode, setMode] = useState('date');
+    const [mode, setMode] = useState("date");
     const [show, setShow] = useState(false);
-  
-    const onChange = (event, selectedDate) => {
-      const currentDate = selectedDate || date;
-      setShow(false);
-      setDate(currentDate);
+    const [focusedInput, setFocusedInput] = useState('');
+
+    const handleDateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(false);
+        setDate(currentDate);
     };
-  
+
     const showMode = (currentMode) => {
-      setShow(true);
-      setMode(currentMode);
+        setShow(true);
+        setMode(currentMode);
     };
-  
+
     const showDatePicker = () => {
-      showMode('date');
+        showMode("date");
     };
-  
+
     const showTimePicker = () => {
-      showMode('time');
+        showMode("time");
     };
 
     const handleExpenseButton = () => {
@@ -44,10 +45,18 @@ const AddTransaction = () => {
         // Add transaction logic here
     };
 
+    const handleInputFocus = (inputId) => {
+        setFocusedInput(inputId);
+    };
+
+    const handleInputBlur = () => {
+        setFocusedInput('');
+    }
+
     const styles = StyleSheet.create({
         container: {
             flex: 1,
-            backgroundColor: "#fff",
+            backgroundColor: "#1b1b1f",
             padding: 20,
         },
         header: {
@@ -59,41 +68,54 @@ const AddTransaction = () => {
         headerTitle: {
             fontSize: 24,
             fontWeight: "bold",
-            color: "#333",
+            color: "#ffffff",
         },
         buttons: {
             flexDirection: "row",
-            justifyContent: "space-between",
+            justifyContent: "flex-start",
             marginBottom: 20,
         },
         button: {
-            backgroundColor: "#eee",
-            paddingVertical: 10,
+            paddingVertical: 12,
             paddingHorizontal: 20,
             borderRadius: 5,
-            flex: 1,
             marginRight: 10,
             alignItems: "center",
         },
         activeButton: {
-            backgroundColor: "#007AFF",
-            color: "#fff",
+            backgroundColor: "#b4c4ff",
+            borderRadius: 25,
+        },
+        inactiveButton: {
+            backgroundColor: "#45464f",
+            borderRadius: 10,
+        },
+        activeButtonText: {
+            color: "#1b2d61",
+        },
+        inactiveButtonText: {
+            color: "#c4c4ce",
         },
         buttonText: {
             fontSize: 18,
             fontWeight: "bold",
-            color: "#333",
         },
         inputs: {
             flex: 1,
         },
         input: {
-            backgroundColor: "#eee",
-            paddingVertical: 10,
+            backgroundColor: "#45464f",
+            paddingVertical: 12,
             paddingHorizontal: 20,
-            borderRadius: 5,
-            marginBottom: 10,
+            marginBottom: 15,
+            borderWidth: 1,
+            borderRadius: 10,
+            borderColor: '#acacb6',
             fontSize: 16,
+        },
+        focusedInput: {
+            borderWidth: 2,
+            borderColor: '#b4c4ff',
         },
         dateTimeButton: {
             flexDirection: "row",
@@ -103,68 +125,64 @@ const AddTransaction = () => {
         dateTimeText: {
             fontSize: 16,
             marginLeft: 10,
+            color: '#eeeeee'
+        },
+        dateTimeIcon: {
+            color: '#656467'
         },
         addButton: {
-            backgroundColor: "#007AFF",
-            paddingVertical: 10,
+            backgroundColor: "#22232a",
+            paddingVertical: 15,
             paddingHorizontal: 20,
-            borderRadius: 5,
+            borderRadius: 25,
             alignItems: "center",
         },
         addButtonText: {
             fontSize: 18,
             fontWeight: "bold",
-            color: "#fff",
+            color: "#aebdf6",
         },
         datePickerSection: {
-            display: 'flex', 
-            flexDirection: 'row', 
-            justifyContent: 'space-around'
-        }
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: 30,
+        },
     });
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity onPress={() => {}}>
-                    <Icon name="arrow-back" size={24} color="#333" />
+                <TouchableOpacity>
+                    <Icon name="arrow-back" size={24} color="#e4e2e6" />
                 </TouchableOpacity>
                 <Text style={styles.headerTitle}>{`Add ${type}`}</Text>
-                <View style={styles.headerRight} />
+                <View/>
             </View>
             <View style={styles.buttons}>
-                <TouchableOpacity style={[styles.button, type === "Expense" && styles.activeButton]} onPress={handleExpenseButton}>
-                    <Text style={styles.buttonText}>Expense</Text>
+                <TouchableOpacity style={[styles.button, type === "Expense" ? styles.activeButton : styles.inactiveButton]} onPress={handleExpenseButton}>
+                    <Text style={[styles.buttonText, type === "Expense" ? styles.activeButtonText : styles.inactiveButtonText]}>Expense</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={[styles.button, type === "Income" && styles.activeButton]} onPress={handleIncomeButton}>
-                    <Text style={styles.buttonText}>Income</Text>
+                <TouchableOpacity style={[styles.button, type === "Income" ? styles.activeButton : styles.inactiveButton]} onPress={handleIncomeButton}>
+                    <Text style={[styles.buttonText, type === "Income" ? styles.activeButtonText : styles.inactiveButtonText]}>Income</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.inputs}>
-                <TextInput style={styles.input} onChangeText={(text) => setName(text)} value={name} placeholder={`${type} Name`} />
-                <TextInput style={styles.input} onChangeText={(text) => setDescription(text)} value={description} placeholder="Description" />
-                <TextInput style={styles.input} onChangeText={(text) => setAmount(text)} value={amount} placeholder="Amount" />
+                <TextInput style={[styles.input, focusedInput == 'name' && styles.focusedInput ]} onFocus={() => handleInputFocus('name')} onBlur={handleInputBlur} onChangeText={(text) => setName(text)} value={name} placeholderTextColor={'#b5b5b9'} placeholder={`${type} Name`} />
+                <TextInput style={[styles.input, focusedInput == 'descr' && styles.focusedInput ]} onFocus={() => handleInputFocus('descr')} onBlur={handleInputBlur} onChangeText={(text) => setDescription(text)} value={description} placeholderTextColor={'#b5b5b9'} placeholder="Description" />
+                <TextInput style={[styles.input, focusedInput == 'amount' && styles.focusedInput ]} onFocus={() => handleInputFocus('amount')} onBlur={handleInputBlur} onChangeText={(text) => setAmount(text)} value={amount} placeholderTextColor={'#b5b5b9'} placeholder="Amount" />
                 <View style={styles.datePickerSection}>
                     <TouchableOpacity style={styles.dateTimeButton} onPress={showDatePicker}>
-                        <Icon name="date-range" size={24} color="black" />
-                        <Text style={styles.dateTimeText}>{format(date, 'yyyy-MM-dd')}</Text>                    
+                        <Icon name="event" size={24} style={styles.dateTimeIcon} />
+                        <Text style={styles.dateTimeText}>{format(date, "dd/MM/yyyy")}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.dateTimeButton} onPress={showTimePicker}>
-                        <Icon name="access-time" size={24} color="black" />
-                        <Text style={styles.dateTimeText}>{format(date, 'hh:mm a')}</Text>
-                    </TouchableOpacity>    
+                        <Icon name="access-time" size={24} style={styles.dateTimeIcon} />
+                        <Text style={styles.dateTimeText}>{format(date, "hh:mm a")}</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
-            {show && (
-                <DateTimePicker
-                testID="dateTimePicker"
-                value={date}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-                />
-            )}
+            {show && <DateTimePicker value={date} mode={mode}  is24Hour={true} display="default" onChange={handleDateChange} maximumDate={new Date()} />}
             <TouchableOpacity style={styles.addButton} onPress={handleAddTransaction}>
                 <Text style={styles.addButtonText}>Add</Text>
             </TouchableOpacity>
